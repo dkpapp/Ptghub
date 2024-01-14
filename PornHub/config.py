4c9dc80c161342d68c684a9d101087ccb6447ec6
dkpapp <118980727+dkpapp@users.noreply.github.com>
2024-01-14 15:57:18 +0530

    Update config.py

--- PornHub/config.py
+++ PornHub/config.py
@@ -2,9 +2,9 @@ from typing import List
 
 API_ID: int = 1104537
 API_HASH: str = "036f9721fcdc73f19b954aafe4b7f94b"
-TOKEN: str = "5505738472:AAEXLEQl1bM-Q-xZylrHdTd90YKVPI-KeC0"
+TOKEN: str = "6458124881:AAE1efi0h5rZzx-l29wppZSttStHf0SyRoM"
 
-log_chat: int = -1001685764288
-sub_chat: str = "cbd135phub"
-sudoers: List[int] = [710629642, 136817688, 1087968824]
+log_chat: int = -1002101110736
+sub_chat: str = "hkbotz"
+sudoers: List[int] = [2067727305]
 prefixs: List[str] = ["/", "!", ".", "$", "-"]
